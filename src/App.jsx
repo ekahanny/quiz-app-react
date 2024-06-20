c0b99bdf6b1e995bc42bf62f8eb0080c7055974c
ekahnny <ekahnny@gmail.com>
2024-06-20 16:25:13 +0000

    menyelesaikan laman home

--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,30 @@ import Card from "./components/elements/Card";
 
 function App() {
 
-  const fontStyleh1 = () => {
+  const fontStyle = () => {
     return {
       fontFamily: "Josefin Sans",
       fontWeight: "700",
       letterSpacing: "1px",
       fontStyle: "italic",
-      fontSize: "40px"
+      fontSize: "40px",
+      color: "#F27BBD",
+      textShadow: "2px 2px 4px rgba(255, 255, 255, 0.1)"
     }
-  } 
-
-  const fontStyleh2 = () => {
-    return {
-      fontFamily: "Playwrite NG Modern",
-      fontSize: "30px",
-      marginTop: "5px"
-
-    }
-  } 
-
+  }
 
   return (
-    <div>
-        <Card>
-            <div className="flex justify-center items-center flex-col text-white">
-              <h1 style={fontStyleh1()}>Welcome to...</h1>
-              <h2 style={fontStyleh2()}>Quiz App</h2>
-              <input type="text" placeholder="Enter your name" className="input input-bordered input-primary w-full max-w-xs mt-5" />
-              <button className="btn btn-outline btn-secondary mt-5">Start</button>
-            </div>
-        </Card>
-    </div>
+      <Card>
+        <div className="flex flex-col items-center justify-center absolute inset-0">
+          <h1 style={fontStyle()}>Quiz App</h1>
+          <input 
+            type="text" 
+            placeholder="Enter your name" 
+            className="w-full max-w-xs my-8 bg-transparent border-b-2 border-[#F27BBD] text-[#F27BBD] placeholder-[#F27BBD] p-2 text-center	focus:outline-none"
+          />
+          <button className="btn btn-outline btn-secondary mt-2">Secondary</button>
+        </div>
+      </Card>
   )
 }
 
