07d4c67e8a275b3019b1d9be62ae891d8d7f1556
ekahnny <ekahnny@gmail.com>
2024-06-22 11:50:10 +0000

    clicked answer

--- src/components/elements/ChoiceAnswer.jsx
+++ src/components/elements/ChoiceAnswer.jsx
@@ -1,32 +1,28 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react";
-import Correct from "./CorrectAnswer";
-import Incorrect from "./IncorrectAnswer";
 
-const ChoiceAnswer = ({ choice, isSelected, isCorrect }) => {
-    const [hover, setHover] = useState(false);
+const ChoiceAnswer = ({ choice, onClick }) => {
+  const [hover, setHover] = useState(false);
 
-    const hoverBackground = {
-        background: 'linear-gradient(#E65895, #BC6BE8)',
-    };
+  const hoverBackground = {
+    background: "linear-gradient(#E65895, #BC6BE8)",
+  };
 
-    const defaultBackground = {
-        background: '#393F6E',
-    };
+  const defaultBackground = {
+    background: "#393F6E",
+  };
 
-    return (
-        <button 
-            className="text-white text-lg px-8 py-4 m-1 rounded-xl flex-grow text-center flex items-center justify-between"
-            style={hover ? hoverBackground : defaultBackground}
-            onMouseOver={() => setHover(true)}
-            onMouseOut={() => setHover(false)}
-        >
-            {choice}
-            {isSelected && (
-                isCorrect ? <Correct /> : <Incorrect />
-            )}
-        </button>
-    );
-}
+  return (
+    <button
+      className="text-white text-lg px-8 py-4 m-1 rounded-xl flex-grow text-center flex items-center justify-between"
+      style={hover ? hoverBackground : defaultBackground}
+      onMouseOver={() => setHover(true)}
+      onMouseOut={() => setHover(false)}
+      onClick={() => onClick(choice)} // Handle click here
+    >
+      {choice}
+    </button>
+  );
+};
 
 export default ChoiceAnswer;
--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable react/jsx-key */
 import { useState, useEffect } from "react";
 import ChoiceAnswer from "../components/elements/Answer";
 import Number from "../components/elements/Number";
@@ -8,54 +6,71 @@ import getQuestions from "../services/question.services";
 import { useParams } from "react-router-dom";
 
 const QuizPages = () => {
-    const [questions, setQuestions] = useState([]);
-    const [questionIndex, setQuestionIndex] = useState(0);
-    const { category, apiCategory } = useParams();
+  const [questions, setQuestions] = useState([]);
+  const [questionIndex, setQuestionIndex] = useState(0);
+  const { category, apiCategory } = useParams();
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const data = await getQuestions(apiCategory);
-                setQuestions(data);
-                console.log("data: ", data);
-            } catch (error) {
-                console.log("Error fetching data", error);
-            }
-        };
-        fetchData();
-    }, [apiCategory]);
-
-    const handleNumberOfQuestion = (index) => {
-        setQuestionIndex(index);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getQuestions(apiCategory);
+        setQuestions(data);
+        console.log("data: ", data);
+      } catch (error) {
+        console.log("Error fetching data", error);
+      }
     };
+    fetchData();
+  }, [apiCategory]);
 
-    return (
-        <Card>
-            <h1 className="text-white text-center mt-3 font-semibold text-sm">{category} Quiz</h1>
-            {/* Number of questions */}
-            <div className="flex justify-center my-5">
-                {questions.map((_, index) => (
-                    <Number key={index} number={index + 1} isActive={index === questionIndex} onClick={() => handleNumberOfQuestion(index)} />
-                ))}
-            </div>
+  const handleNumberOfQuestion = (index) => {
+    setQuestionIndex(index);
+  };
 
-            {/* Question */}
-            {questions.length > 0 && (
-                <p className="text-white text-center text-2xl mt-6">{questions[questionIndex].question}</p>
-            )}
+  const handleClickAnswer = (answer) => {
+    console.log("Selected answer:", answer);
+  };
 
-            {/* Choice Answer */}
-            <div className="grid grid-cols-2 gap-4 mx-28 my-10">
-                {questions.length > 0 &&
-                    questions[questionIndex].incorrectAnswers.map((choice, index) => (
-                        <ChoiceAnswer key={index} choice={choice} />
-                    ))}
-                {questions.length > 0 && (
-                    <ChoiceAnswer choice={questions[questionIndex].correctAnswer} />
-                )}
-            </div>
-        </Card>
-    );
+  return (
+    <Card>
+      <h1 className="text-white text-center mt-3 font-semibold text-sm">{category} Quiz</h1>
+
+      {/* Number of questions */}
+      <div className="flex justify-center my-5">
+        {questions.map((_, index) => (
+          <Number
+            key={index}
+            number={index + 1}
+            isActive={index === questionIndex}
+            onClick={() => handleNumberOfQuestion(index)}
+          />
+        ))}
+      </div>
+
+      {/* Question */}
+      {questions.length > 0 && (
+        <p className="text-white text-center text-2xl mt-6">{questions[questionIndex].question}</p>
+      )}
+
+      {/* Choice Answer */}
+      <div className="grid grid-cols-2 gap-4 mx-28 my-10">
+        {questions.length > 0 &&
+          questions[questionIndex].incorrectAnswers.map((choice, index) => (
+            <ChoiceAnswer
+              key={index}
+              choice={choice}
+              onClick={() => handleClickAnswer(choice)}
+            />
+          ))}
+        {questions.length > 0 && (
+          <ChoiceAnswer
+            choice={questions[questionIndex].correctAnswer}
+            onClick={() => handleClickAnswer(questions[questionIndex].correctAnswer)}
+          />
+        )}
+      </div>
+    </Card>
+  );
 };
 
 export default QuizPages;
